81cb087655bae0d25ae9dd7ec8e373fe5f439257
wachirahmuturi <wachiramuturi18@gmail.com>
2022-10-18 12:17:51 +0000

    rename file correctly

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /**
-  * _putchar - writes the character c to stdout
+  * int _putchar - writes the character c to stdout
   * @c: The character to print
   *
   * Return: On success 0.
@@ -30,7 +30,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 
 /**
- * _isalpha - checks for alphabet character
+ * int _isalpha - checks for alphabet character
  * @c: character to check
  * Return: 1 if lower or uppercase, otherwise 0 if not alphabet character
  */
@@ -38,7 +38,7 @@ int _islower(int c);
 int _isalpha(int c);
 
 /**
-  * print_sign(int n) - prints the sign of a number
+  * int print_sign(int n) - prints the sign of a number
   * @n: number to be checked
   * Return: 1 if (n>0), 0 if (n=0), -1 if (n<0)
   */
@@ -52,7 +52,7 @@ int print_sign(int n);
 int _abs(int);
 
 /**
- * print_last_digit - print last digit of integer
+ * int print_last_digit - print last digit of integer
  * @int: integer
  * Return: last digit of number
  */
@@ -60,19 +60,19 @@ int _abs(int);
 int print_last_digit(int);
 
 /**
- * jack_bauer - print every minute of a day
+ * void jack_bauer - print every minute of a day
  * Description: print minutes 00:00 to 23:59
  */
 void jack_bauer(void);
 
 /**
- * times_table - print multiplication table
+ * void times_table - print multiplication table
  */
 
 void times_table(void);
 
 /**
- * add - add two integers
+ * int add - add two integers
  * @a: two integer arguments
  * @b: two integer arguments
  * Return: sum
@@ -81,21 +81,21 @@ void times_table(void);
 int add(int a, int b);
 
 /**
- * print_to_98 - print numbers n to 98
+ * void print_to_98 - print numbers n to 98
  * @n: integer argument
  */
 
 void print_to_98(int n);
 
 /**
- * print_times_table - print multiplication table up to n
+ * void print_times_table - print multiplication table up to n
  * @n: integer argument
  */
 
 void print_times_table(int n);
 
 /**
-  * multiples_3_or_5 - print all the multiples of 3 or 5
+  * int multiples_3_or_5 - print all the multiples of 3 or 5
   * below 1024 followed by a new line
   * Return: always 0
   */
