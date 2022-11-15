86851720c7e42159183a908f2f3cdde7b4687a77
wachirahmuturi <wachiramuturi18@gmail.com>
2022-11-15 18:00:31 +0000

    Using struct anf typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+  * struct dog - prints dog description
+  * @name: name of the dog
+  * @age: dog's age
+  * @owner: the dog's owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
